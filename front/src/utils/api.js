b0b887070602bffc15b2ba8c566ead6148de4826
ThomasHurtado <thomashurtado@outlook.com.br>
2024-06-22 11:53:21 -0300

    arrumando erro

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -1,11 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-<<<<<<< HEAD
     //baseURL: 'https://loja-online-web.vercel.app/',
     baseURL: 'http://localhost:5000/',
 })
-=======
-    baseURL: 'https://loja-online-alpha.vercel.app/',
-})
->>>>>>> 1ec8d52e42001a2bb62c7b28c0b3fde9d6616421
